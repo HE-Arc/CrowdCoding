90ebb568f7580effa3b6510c7aba8bba3dc0e244
Maxime <maximegrava@gmail.com>
2019-04-17 14:51:53 +0200

    create snippetRepository

---
+++ src/main/java/ch/arc/crowdcoding/repository/SnippetRepository.java
@@ -0,0 +1,19 @@
+package ch.arc.crowdcoding.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.arc.crowdcoding.model.CodeSnippet;
+import ch.arc.crowdcoding.model.User;
+
+
+@Repository
+public interface SnippetRepository extends PagingAndSortingRepository<CodeSnippet, Integer> {
+	
+	Page<CodeSnippet> findByOwner(User owner, Pageable pageable);
+	
+}
