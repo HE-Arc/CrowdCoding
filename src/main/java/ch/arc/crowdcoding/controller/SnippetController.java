1b6756ce86ac04a837067cae14b0f17177c02ca1
Naralas <ludovic.herbelin@gmail.com>
2019-04-17 13:21:57 +0200

    Added request methods as sonar cloud suggested

--- src/main/java/ch/arc/crowdcoding/controller/SnippetController.java
+++ src/main/java/ch/arc/crowdcoding/controller/SnippetController.java
@@ -3,19 +3,20 @@ package ch.arc.crowdcoding.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/snippets")
 public class SnippetController {
 	
-	@RequestMapping(value="/list")
+	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public String snippetsList()
 	{
 		return "snippets/list";
 	}
 	
-	@RequestMapping("/new")
+	@RequestMapping(value="/new", method=RequestMethod.GET)
 	public String getNewSnippetForm()
 	{
 		return "snippets/new";
