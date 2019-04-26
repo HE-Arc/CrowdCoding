b4a761d2b0575d7ca23a509246b7b2bee8a9c27a
Naralas <ludovic.herbelin@gmail.com>
2019-04-26 15:45:27 +0200

    Added checks for code modification and execution. Updated code executions with structure for other languages (not implemented)

--- src/main/java/ch/arc/crowdcoding/controller/SnippetController.java
+++ src/main/java/ch/arc/crowdcoding/controller/SnippetController.java
@@ -32,110 +32,126 @@ import ch.arc.crowdcoding.service.UserService;
 @Controller
 @RequestMapping(value = "/snippets")
 public class SnippetController {
-	
+
 	@Autowired
 	private SnippetService snippetService;
 	@Autowired
 	private SnippetExecutionService snippetExecutor;
-	
+
 	@Autowired
 	private UserService userService;
 	@Autowired
 	private SecurityService securityService;
-	
+
 	@Autowired
 	private LanguageService languageService;
-	
-	
-	
-	@RequestMapping(value="/list", method=RequestMethod.GET)
-	public RedirectView snippetsListEmpty()
-	{
+
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public RedirectView snippetsListEmpty() {
 		return new RedirectView("/snippets/list/0");
 	}
-	
-	@RequestMapping(value="/list/{page}", method=RequestMethod.GET)
-	public ModelAndView snippetsList(@PathVariable(value="page") Integer page)
-	{
-		if(page < 0 )
+
+	@RequestMapping(value = "/list/{page}", method = RequestMethod.GET)
+	public ModelAndView snippetsList(@PathVariable(value = "page") Integer page) {
+		if (page < 0)
 			page = 0;
-		
+
 		Pageable pageable = PageRequest.of(page, 4, Sort.by("createdAt").descending());
-    	Page<CodeSnippet> snippets = snippetService.findByAccessibility("Public", pageable);
-   
-    	ModelAndView mav = new ModelAndView("snippets/list");
-    	mav.addObject("snippets", snippets);
-    	if(snippets.getTotalPages()-1 > page)
-    		mav.addObject("nextPage", page+1);
-    	if(page > 0)
-    		mav.addObject("previousPage", page-1);
-    	
+		Page<CodeSnippet> snippets = snippetService.findByAccessibility("Public", pageable);
+
+		ModelAndView mav = new ModelAndView("snippets/list");
+		mav.addObject("snippets", snippets);
+		if (snippets.getTotalPages() - 1 > page)
+			mav.addObject("nextPage", page + 1);
+		if (page > 0)
+			mav.addObject("previousPage", page - 1);
+
 		return mav;
 	}
-	
-	@RequestMapping(value="/new", method=RequestMethod.GET)
-	public ModelAndView getNewSnippetForm()
-	{
+
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public ModelAndView getNewSnippetForm() {
 		ModelAndView mav = new ModelAndView("snippets/new");
 		mav.addObject("newCodeSnippet", new CodeSnippet());
 		mav.addObject("languages", languageService.findAll());
-		
+
 		return mav;
 	}
-	
-	//Add to db and redirect to modifiy
-	@RequestMapping(value="/add", method=RequestMethod.POST)
-	public RedirectView addNewSnippet(
-			@RequestParam("snippet_name") String name,
+
+	// Add to db and redirect to modifiy
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public RedirectView addNewSnippet(@RequestParam("snippet_name") String name,
 			@RequestParam("snippet_language") String language,
-			@RequestParam("snippet_accessibility") String accessibility)
+			@RequestParam("snippet_accessibility") String accessibility) {
+
+		User currentUser = getCurrentUser();
+
+		CodeSnippet snippet = snippetService.createNewSnippet(currentUser, name,
+				languageService.findByLanguage(language), accessibility);
+
+		return new RedirectView("/snippets/" + snippet.getId() + "/edit");
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView saveSnippet(@RequestParam("snippet_id") Integer id, @RequestParam("snippet_name") String name,
+			@RequestParam("snippet_content") String content, @RequestParam("snippet_user") Integer user,
+			@RequestParam("snippet_language") String language, @RequestParam("snippet_accessibility") String access) {
+
+		CodeSnippet targetSnippet = snippetService.findById(id);
+		
+		// change it only if it's the owner
+		String updateAccessibility = targetSnippet.getAccessibility();
+		if(checkSnippetIsOwner(targetSnippet))
+			updateAccessibility = access; 
+		
+		snippetService.updateSnippet(id, name, content, userService.findUserById(user),
+				languageService.findByLanguage(language), updateAccessibility);
+
+		return null;// "snippets/"+snippet.getId()+"/edit";
+	}
+
+	@RequestMapping("/{id}/edit")
+	public ModelAndView getEditorSnippetForm(@PathVariable(value = "id") Integer id) {
+
+		CodeSnippet snippet = snippetService.findById(id);
+		if (snippet == null || !checkSnippetAccessible(snippet))
+			return new ModelAndView("error/404");
+
+		ModelAndView mav = new ModelAndView("snippets/edit");
+		mav.addObject("snippet", snippet);
+		mav.addObject("languages", languageService.findAll());
+
+		return mav;
+	}
+
+	@RequestMapping(value = "{id}/execute", method = RequestMethod.GET, produces = "application/json")
+	@ResponseBody
+	public String executeSnippet(@PathVariable(value = "id") Integer id) {
+		CodeSnippet codeSnippet = snippetService.findById(id);
+		String jsonOutput = "";
+
+		if (codeSnippet != null && checkSnippetAccessible(codeSnippet))
+			jsonOutput = snippetExecutor.runSnippet(codeSnippet);
+		return jsonOutput;
+	}
+
+	private boolean checkSnippetAccessible(CodeSnippet targetSnippet) {
+		return targetSnippet.getAccessibility().equals("Public")
+				|| checkSnippetIsOwner(targetSnippet);
+	}
+	
+	private boolean checkSnippetIsOwner(CodeSnippet targetSnippet)
 	{
-		//Current authenticated user 
-		//Maybe check if currentUser is not defined ?
+		return targetSnippet.getOwner().getId().equals(getCurrentUser().getId());
+	}
+
+	private User getCurrentUser() {
+		// Current authenticated user
+		// Maybe check if currentUser is not defined ?
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		String currentPrincipalName = authentication.getName();
 		User currentUser = userService.findUserByName(currentPrincipalName);
-    	
-    	CodeSnippet snippet = snippetService.createNewSnippet(currentUser, name, languageService.findByLanguage(language), accessibility);
-		
-		return new RedirectView("/snippets/"+snippet.getId()+"/edit");
-	}
-	
-	@RequestMapping(value="/save", method=RequestMethod.POST)
-	public ModelAndView saveSnippet(@RequestParam("snippet_id") Integer id, @RequestParam("snippet_name") String name,
-								@RequestParam("snippet_content") String content, @RequestParam("snippet_user") Integer user,
-								@RequestParam("snippet_language") String language, @RequestParam("snippet_accessibility") String access)
-	{    	
-		CodeSnippet snippet= snippetService.updateSnippet(id, name, content, userService.findUserById(user), languageService.findByLanguage(language), access);
-		
-		if(snippet==null)
-			return new ModelAndView("error/404"); 
-		
-		return null;// "snippets/"+snippet.getId()+"/edit";
-	}
-	
-	@RequestMapping("/{id}/edit")
-	public ModelAndView getEditorSnippetForm(@PathVariable(value="id") Integer id)
-	{
-		CodeSnippet snippet = snippetService.findById(id);
-		if(snippet == null)
-			return new ModelAndView("error/404");
-		
-		ModelAndView mav = new ModelAndView("snippets/edit");
-        mav.addObject("snippet", snippet);
-		mav.addObject("languages", languageService.findAll());
-        
-		return mav;
-	}
-	
-	@RequestMapping(value="{id}/execute", method=RequestMethod.GET, produces = "application/json")
-	@ResponseBody public String executeSnippet(@PathVariable(value="id") Integer id)
-	{
-		CodeSnippet codeSnippet = snippetService.findById(id);
-		String jsonOutput = "";
-		
-		if(codeSnippet != null)
-			jsonOutput = snippetExecutor.runSnippet(codeSnippet);
-		return jsonOutput;
+
+		return currentUser;
 	}
 }
